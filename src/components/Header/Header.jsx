5128bf5e528ba734e6849e13f26a36e1b2b7559f
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2025-02-28 07:41:12 +0100

    feat: work on main header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,11 +27,19 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 24px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: center;
+  position: relative;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  flex: 3;
+  justify-content: flex-start;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -14,6 +14,7 @@ const Logo = (props) => {
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1;
 `;
 
 const Wrapper = styled.h1`
